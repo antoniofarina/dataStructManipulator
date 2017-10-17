0eba2aef1e3a7f428c947c228d2305dfb95b7be3
Antonio Farina <antonio@translated.net>
2017-10-17 10:58:34 +0200

    initialized repo -->  first commit

---
+++ dataStructureManipulator/Test/dataStructureManipulatorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace dataStructureManipulator\Test;
+require_once "../dataStructureManipulator.php";
+
+use \dataStructureManipulator\dataStructureManipulator;
+
+class dataStructureManipulatorTest extends \PHPUnit_Framework_TestCase
+{
+    //
+
+	public function testFlatIntegerArrayReturnsFlattedArray()
+    {
+        $originalInput = array(1,4,5,array(4,5,6),456,4,2,array(34,5,array(67)));
+        $expectedResult = array(1,4,5,4,5,6,456,4,2,34,5,67);
+
+        $dataStructureManipulator = new dataStructureManipulator();
+        $flatArrayResult = $dataStructureManipulator->flatIntegerArray($originalInput);
+        $this->assertEquals($expectedResult, $flatArrayResult);
+    }
+
+
+}
---
+++ dataStructureManipulator/dataStructureManipulator.php
@@ -0,0 +1,16 @@
+<?
+
+namespace dataStructureManipulator;
+
+class dataStructureManipulator {
+	public function __construct(){
+		;
+	}
+
+	public function flatIntegerArray ($inputArray=null) {
+		$flatedArray=array();
+
+		return $flatedArray;
+	}
+
+}
