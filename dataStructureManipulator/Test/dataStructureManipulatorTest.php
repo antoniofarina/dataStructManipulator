8d1602e31f3ff7a7721490a7dfdae148df487ca2
Antonio Farina <antonio@translated.net>
2017-10-17 18:10:41 +0200

    added comments to the Test class

--- dataStructureManipulator/Test/dataStructureManipulatorTest.php
+++ dataStructureManipulator/Test/dataStructureManipulatorTest.php
@@ -11,12 +11,23 @@
 *
 * @access   public
 */
-
 namespace dataStructureManipulator\Test;
 use \dataStructureManipulator\dataStructureManipulator;
 
+
+
 class dataStructureManipulatorTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+    * Private function that abstract the common path for all the assertEqual test cases
+    *
+    *
+    * @param array $originalInput. The unflatted input
+    *
+    * @param array $expectedResult. The flatted array that we expect to obtain in case of success
+    *
+    * @return void
+    */
     private function __assertEqual($originalInput,$expectedResult){
         $flatArrayResult=array();
         dataStructureManipulator::flatIntegerArray($originalInput,$flatArrayResult);
@@ -24,6 +35,14 @@ class dataStructureManipulatorTest extends \PHPUnit_Framework_TestCase
     }
 
 
+    /**
+    * Test the happy case: the input is a nested array of only integers items
+    *
+    * @param void
+    * @param void
+    *
+    * @return void
+    */
     public function testFlatIntegerArrayReturnsFlattedArray()
     {
         $originalInput = array(1,4,5,array(4,5,6),456,4,2,array(34,5,array(67)));
@@ -31,7 +50,14 @@ class dataStructureManipulatorTest extends \PHPUnit_Framework_TestCase
         $this->__assertEqual($originalInput,$expectedResult);
     }
 
-
+    /**
+    * Test if one of the nested array in the input is empty
+    *
+    * @param void
+    * @param void
+    *
+    * @return void
+    */
     public function testFlatIntegerArrayOnEmptyInnerArray(){
         $originalInput=array(1,4,5,array(),456,4,2,array(34,5,6));
         $expectedResult = array(1,4,5,456,4,2,34,5,6);
@@ -39,7 +65,14 @@ class dataStructureManipulatorTest extends \PHPUnit_Framework_TestCase
     }
 
 
-
+    /**
+    * Test if one of the nested array contains  null element
+    *
+    * @param void
+    * @param void
+    *
+    * @return void
+    */
     public function testFlatIntegerArrayOnNullInnerArray(){
         $originalInput=array(1,4,5,array(null),456,4,2,array(34,5,6));
         $expectedResult = array(1,4,5,456,4,2,34,5,6);
@@ -47,19 +80,45 @@ class dataStructureManipulatorTest extends \PHPUnit_Framework_TestCase
     }
 
 
+    /**
+    * Test if one of the items in the input array is a non numeric string
+    * A RuntimeException  is expected
+    *
+    * @param void
+    * @param void
+    *
+    * @return void
+    */
     public function testFlatIntegerArrayOnNonNumericArgument () {
         $originalInput = array(1,4,5,array(4,5,6),'string',4,2,array(34,5,array(67)));
         $this->expectException(\RuntimeException::class);
         dataStructureManipulator::flatIntegerArray($originalInput,$flatArrayResult);
     }
 
+    /**
+    * Test if the input is not an array
+    * A RuntimeException  is expected
+    *
+    * @param void
+    * @param void
+    *
+    * @return void
+    */
     public function testFlatIntegerArrayOnNonArrayInput () {
         $originalInput = 1;
         $this->expectException(\RuntimeException::class);
         dataStructureManipulator::flatIntegerArray($originalInput,$flatArrayResult);
     }
 
-
+    /**
+    * Test if one of the items in the input array is a  numeric string
+    * A RuntimeException  is expected
+    *
+    * @param void
+    * @param void
+    *
+    * @return void
+    */
     public function testFlatIntegerArrayOnNumericStringArgument () {
         $originalInput = array(1,4,5,array(4,5,6),'1234',4,2,array(34,5,array(67)));
         $expectedResult = array(1,4,5,4,5,6,1234,4,2,34,5,67);
