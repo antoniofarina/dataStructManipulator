6c0968748f2d3e7279058a02fdc473c70ffd3419
Antonio Farina <antonio@translated.net>
2017-10-17 16:40:43 +0200

    added test cases on following input - null inner input - empty inner input - numeric string inner input - non numeric string input - happy case input: nested array of only integers

--- dataStructureManipulator/Test/dataStructureManipulatorTest.php
+++ dataStructureManipulator/Test/dataStructureManipulatorTest.php
@@ -6,17 +6,47 @@ use \dataStructureManipulator\dataStructureManipulator;
 
 class dataStructureManipulatorTest extends \PHPUnit_Framework_TestCase
 {
-    //
-
-	public function testFlatIntegerArrayReturnsFlattedArray()
-    {
-        $originalInput = array(1,4,5,array(4,5,6),456,4,2,array(34,5,array(67)));
-        $expectedResult = array(1,4,5,4,5,6,456,4,2,34,5,67);
-
-        $dataStructureManipulator = new dataStructureManipulator();
-        $flatArrayResult = $dataStructureManipulator->flatIntegerArray($originalInput);
+    private function __assertEqual($originalInput,$expectedResult){
+        $flatArrayResult=array();
+        dataStructureManipulator::flatIntegerArray($originalInput,$flatArrayResult);
         $this->assertEquals($expectedResult, $flatArrayResult);
     }
 
 
+    public function testFlatIntegerArrayReturnsFlattedArray()
+    {
+        $originalInput = array(1,4,5,array(4,5,6),456,4,2,array(34,5,array(67)));
+        $expectedResult = array(1,4,5,4,5,6,456,4,2,34,5,67);
+        $this->__assertEqual($originalInput,$expectedResult);
+    }
+
+
+    public function testFlatIntegerArrayOnEmptyInnerArray(){
+        $originalInput=array(1,4,5,array(),456,4,2,array(34,5,6));
+        $expectedResult = array(1,4,5,456,4,2,34,5,6);
+        $this->__assertEqual($originalInput,$expectedResult);
+    }
+
+
+
+    public function testFlatIntegerArrayOnNullInnerArray(){
+        $originalInput=array(1,4,5,array(null),456,4,2,array(34,5,6));
+        $expectedResult = array(1,4,5,456,4,2,34,5,6);
+        $this->__assertEqual($originalInput,$expectedResult);
+    }
+
+
+    public function testFlatIntegerArrayOnNonNumericArgument () {
+        $originalInput = array(1,4,5,array(4,5,6),'string',4,2,array(34,5,array(67)));
+        $this->expectException(\RuntimeException::class);
+        dataStructureManipulator::flatIntegerArray($originalInput,$flatArrayResult);
+    }
+
+
+    public function testFlatIntegerArrayOnNumericStringArgument () {
+        $originalInput = array(1,4,5,array(4,5,6),'1234',4,2,array(34,5,array(67)));
+        $expectedResult = array(1,4,5,4,5,6,1234,4,2,34,5,67);
+        $this->__assertEqual($originalInput,$expectedResult);
+
+    }
 }
