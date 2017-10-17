b1689bb2138bfb120d8e459aa4aac9862315f9ec
Antonio Farina <antonio@translated.net>
2017-10-17 16:53:06 +0200

    implemented class with the only static method flatIntegerArray

--- dataStructureManipulator/dataStructureManipulator.php
+++ dataStructureManipulator/dataStructureManipulator.php
@@ -1,16 +1,39 @@
 <?
-
 namespace dataStructureManipulator;
 
+
 class dataStructureManipulator {
-	public function __construct(){
-		;
-	}
+    public static function flatIntegerArray ($inputArray=null,&$flattedArray=null) {
+        if (!is_array($inputArray)){
+            throw new \RuntimeException("The input is not an array.");
+        }
 
-	public function flatIntegerArray ($inputArray=null) {
-		$flatedArray=array();
+        if (empty($inputArray)) {
+            return $inputArray;
+        }
 
-		return $flatedArray;
-	}
+        
+        foreach ($inputArray as  $key => $item) {
+            if (is_integer($item)){
+                $flattedArray[]=$item;
+                continue;
+            }
+
+            if (is_null($item) or empty($item)){
+                continue;
+            }
+
+            if (is_numeric($item) and ((float)$item==(int)$item)){
+                $flattedArray[]=(int)$item;
+                continue;
+            }
+
+            if (is_array($item)){
+                self::flatIntegerArray($item,$flattedArray);
+                continue;
+            }
+            throw new \RuntimeException("The element at position $key is not an integer\n ". print_r($item,true)."\n");
+        }
+    }
 
 }
